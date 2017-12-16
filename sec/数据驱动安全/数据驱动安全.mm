f3d848ac465a7645b69243a2156d864871b5c13e
silence <imsilence@outlook.com>
2017-12-16 11:36:05 +0800

    commit
    Signed-off-by: silence <imsilence@outlook.com>

---
+++ sec/数据驱动安全/数据驱动安全.mm
@@ -0,0 +1,31 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1500616570441" ID="ID_1376223512" MODIFIED="1500616624463" TEXT="&#x6570;&#x636e;&#x9a71;&#x52a8;&#x5b89;&#x5168;">
+<node CREATED="1503708330929" ID="ID_291271224" MODIFIED="1503708334221" POSITION="right" TEXT="01">
+<node CREATED="1503708335288" ID="ID_1264689435" MODIFIED="1503708410181" TEXT="&#x901a;&#x5411;&#x6570;&#x636e;&#x9a71;&#x52a8;&#x5b89;&#x5168;&#x7684;&#x65c5;&#x9014;">
+<node CREATED="1503708410103" ID="ID_1945539701" MODIFIED="1503708412919" TEXT="&#x76ee;&#x7684;">
+<node CREATED="1503708349773" ID="ID_1396769829" MODIFIED="1503708389983" TEXT="&#x6570;&#x636e;&#x5206;&#x6790;&#x548c;&#x53ef;&#x89c6;&#x5316;&#x7684;&#x76ee;&#x7684;&#x662f;&#x4ece;&#x4e2d;&#x83b7;&#x53d6;&#x65b0;&#x7684;&#x89c2;&#x70b9;&#xff0c;&#x53d1;&#x73b0;&#x4e00;&#x4e9b;&#x6570;&#x636e;&#x4e4b;&#x95f4;&#x7684;&#x65b0;&#x5173;&#x7cfb;&#xff0c;&#x6216;&#x53d1;&#x73b0;&#x4e00;&#x4e9b;&#x65b0;&#x4fe1;&#x606f;"/>
+<node CREATED="1503708413925" ID="ID_112387947" MODIFIED="1503708449353" TEXT="&#x4e86;&#x89e3;&#x4fe1;&#x606f;&#x7cfb;&#x7edf;&#x5982;&#x4f55;&#x8fd0;&#x884c;&#xff0c;&#x5f53;&#x53d1;&#x751f;&#x6545;&#x969c;&#xff0c;&#x77e5;&#x9053;&#x6545;&#x969c;&#x5982;&#x4f55;&#x4ea7;&#x4ee5;&#x53ca;&#x5982;&#x4f55;&#x4fee;&#x590d;"/>
+<node CREATED="1503708467635" ID="ID_1638413022" MODIFIED="1503708487792" TEXT="&#x901a;&#x8fc7;&#x6570;&#x636e;&#x5206;&#x6790;&#x79d1;&#x5b66;&#x68c0;&#x9a8c;&#x548c;&#x652f;&#x6491;&#x4eba;&#x7684;&#x76f4;&#x89c9;&#x89c2;&#x70b9;"/>
+</node>
+<node CREATED="1503709553479" ID="ID_751444966" MODIFIED="1503709556178" TEXT="&#x57fa;&#x672c;&#x6280;&#x80fd;">
+<node CREATED="1503709557186" ID="ID_1029482083" MODIFIED="1503709568216" TEXT="&#x9886;&#x57df;&#x4e13;&#x4e1a;&#x652f;&#x6301;">
+<node CREATED="1503709593110" ID="ID_1713290293" MODIFIED="1503709600826" TEXT="&#x8bbe;&#x7f6e;&#x548c;&#x7ef4;&#x6301;&#x4e00;&#x4e2a;&#x5206;&#x6790;&#x76ee;&#x7684;"/>
+</node>
+<node CREATED="1503709568692" ID="ID_1862797874" MODIFIED="1503709571305" TEXT="&#x6570;&#x636e;&#x7ba1;&#x7406;">
+<node CREATED="1503709602167" ID="ID_140889568" MODIFIED="1503709611497" TEXT="&#x80fd;&#x591f;&#x51c6;&#x5907;&#x3001;&#x5b58;&#x50a8;&#x548c;&#x7ef4;&#x62a4;&#x6570;&#x636e;"/>
+</node>
+<node CREATED="1503709571550" ID="ID_1853011938" MODIFIED="1503709581645" TEXT="&#x7f16;&#x7a0b;&#x6280;&#x80fd;">
+<node CREATED="1503709612746" ID="ID_18847505" MODIFIED="1503709623700" TEXT="&#x5c06;&#x6570;&#x636e;&#x8fde;&#x63a5;&#x5230;&#x5206;&#x6790;&#x7ebd;&#x5e26;"/>
+</node>
+<node CREATED="1503709581997" ID="ID_1770882697" MODIFIED="1503709585284" TEXT="&#x7edf;&#x8ba1;&#x5b66;">
+<node CREATED="1503709625016" ID="ID_1641606857" MODIFIED="1503709629520" TEXT="&#x4ece;&#x6570;&#x636e;&#x4e2d;&#x5b66;&#x4e60;"/>
+</node>
+<node CREATED="1503709585517" ID="ID_100233527" MODIFIED="1503709587383" TEXT="&#x53ef;&#x89c6;&#x5316;">
+<node CREATED="1503709630712" ID="ID_1068730746" MODIFIED="1503709640201" TEXT="&#x4e0e;&#x7ed3;&#x679c;&#x6709;&#x6548;&#x6c9f;&#x901a;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ 软技能.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1508205603031" ID="ID_1389632576" MODIFIED="1508205610651" TEXT="&#x8f6f;&#x6280;&#x80fd;"/>
+</map>
